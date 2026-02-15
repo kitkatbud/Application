119c87abd2585cf92fa2b8276fb40f6dac36a403
kitkatbud <kitkatbuddy11@gmail.com>
2026-02-15 20:08:05 +0000

    added

--- code/script.js
+++ code/script.js
@@ -3,7 +3,16 @@ document.addEventListener("DOMContentLoaded", () => {
 
   form.addEventListener("submit", (e) => {
     e.preventDefault()
-    alert("submit fired")
+    
+    // grab values if you want
+    const name = e.target[0].value
+    const email = e.target[1].value
+
+    // store in sessionStorage to show on payment page
+    sessionStorage.setItem("name", name)
+    sessionStorage.setItem("email", email)
+
+    // go straight to payment page
     window.location.href = "payment.html"
   })
 })
